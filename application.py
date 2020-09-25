053f9b529257284c0e3d7a9dd6dc1864c1857376
Thanh Bình Võ <thanhbinh322000@gmail.com>
2020-09-25 12:08:42 +0300

    add comment in each file

--- application.py
+++ application.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "binhvo"
 socketio = SocketIO(app)
 
+# Create 2 lists and a dict for controlling
+# all of the channels, users and messages for each channel
 channels = []
 users = []
 messages = dict()
@@ -19,68 +21,90 @@ messages = dict()
 @app.route("/")
 @login_required
 def index():
+    """Show the home with all of channel whenever user logged in"""
     return render_template("index.html", channels=channels)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
-    # Forget any user_id
+    # Forget any username
     session.clear()
+    # Get the username from the form
     username = request.form.get("username")
     if request.method == "POST":
+        # Ensure username was submitted
         if not username:
             return render_template("apology.html", message="you must provide a display name!")
+        # Ensure each username is different
         if username in users:
             return render_template("apology.html", message="display name already exists!")
-        
+        # Add username to the global users list
         users.append(username)
+        # Remember which user has logged in
         session['username'] = username
+        # Remember the useron a cookie even if the browser is closed.
         session.permanent = True
-
+        # Redirect user to home page
         return redirect("/")
+    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
 
 @app.route("/logout", methods=['GET'])
 def logout():
+    """Log user in"""
+    # When user log out, try to remove the user from the global list.
     try:
         users.remove(session['username'])
     except ValueError:
         pass
-
+    # Forget any username
     session.clear()
+    # Redirect to the login form
     return redirect("/")
 
 
-@app.route("/create", methods=["POST"])
+@app.route("/create", methods=["POST","GET"])
 def create():
+    """ Create a channel and redirect to its page"""
+
+    # Get the new channel from the form
     new_channel = request.form.get("channel")
     if request.method == "POST":
+        # Make sure user type in a channel name
         if not new_channel:
             return render_template("apology.html", message="you must provide a channel name!")
-        if new_channel in channels:
-            return render_template("apology.html", message="channel name already exits!")
-        
+        # Then add that new channel into the global channel list
         channels.append(new_channel)
+
+        # Add channel to global dict of channels with messages
+        # Every channel is a deque to use popleft() method 
         messages[new_channel] = deque()
 
+        #Then redirect to the new page.
         return redirect("/channels/" + new_channel)
     else:
         return render_template("index.html", channels=channels)
 
-@app.route("/channels/<channel>", methods=["GET","POST"])
+@app.route("/channels/<channel>", methods=["GET"])
 @login_required
 def enter(channel):
+    """Show the user the channel page"""
+
+    #Update the current channel
     session["current_channel"] = channel
     if request.method == "POST":
         return redirect("/")
     else:
-        return render_template("channel.html", channels=channels, messages=messages)
+        return render_template("channel.html", channels=channels, messages=messages[channel])
 
 @socketio.on("joined", namespace='/')
 def joined():
+    """ Send message to announce that user has entered the chat """
+
+    # Get the current channel and save it into 'room'.
     room = session.get('current_channel')
 
     join_room(room)
@@ -88,31 +112,38 @@ def joined():
     emit('status', {
         'users': session.get('username'),
         'channel': room,
-        'msg': session.get('username') + "has entered the chat"},
-        room = room)
+        'msg': session.get('username') + " has entered the chat"},
+        room=room)
 
 @socketio.on("left", namespace='/')
 def left():
-    room =  seesion.get('current_channel')
+    """ Send message to announce that user has left the chat """
+
+    # Get the current channel and save it into 'room'.
+    room =  session.get('current_channel')
 
     leave_room(room)
 
     emit('status', {
-        'msg': session.get('username') + "just left the chat"},
-        room = room)
+        'channel': room,
+        'msg': session.get('username') + " just left the chat"}, 
+        room=room)
+
 
 @socketio.on("send message")
 def send_msg(msg, timestamp):
+    """"Broadcase the messages onto the channel"""
 
     room = session.get('current_channel')
 
+    # Only store the 100 most recent messages per channel, if more than 100 then delete.
     if len(messages[room]) > 100:
         messages[room].popleft()
 
     messages[room].append([timestamp, session.get('username'), msg])
-    
+
     emit('announce message', {
         'user': session.get('username'),
         'timestamp': timestamp,
-        'msg': msg},
-        room = room)
+        'msg': msg}, 
+        room=room)
--- static/scripts.js
+++ static/scripts.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    // When connected, configure button
     socket.on('connect', () => {
-
+        // Notify joined event.
         socket.emit('joined');
 
+        // when user click +channel, remove the local storage
         document.querySelector('#newchannel').addEventListener('click', () => {
             localStorage.removeItem('last_channel');
         });
-
+        // when user leave the channel, then send a notification and remove local storage
         document.querySelector('#leave').addEventListener('click', () => {
 
             socket.emit('left');
@@ -17,43 +19,44 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.replace('/');
         });
 
-
+        // When user logout, also remove local storage
         document.querySelector('#logout').addEventListener('click', () => {
             localStorage.removeItem('last_channel');
         });
 
+        //if the user press 'Enter' = clicking the send button
         document.querySelector('#comment').addEventListener("keydown", event => {
             if (event.key == "Enter") {
                 document.getElementById("send-button").click();
             }
         });
 
+        //The send button emits a "send message" event
         document.querySelector('#send-button').addEventListener("click", () => {
-
+            // save the timestamp
             let timestamp = new Date;
             timestamp = timestamp.toLocaleTimeString();
-
+            // save the content of message
             let msg = document.getElementById("comment").value;
 
             socket.emit('send message', msg, timestamp);
-
+            // after sending a message, clear the message field
             document.getElementById("comment").value = '';
         });
     });
 
+    //  Broadcast the message
     socket.on('status', data => {
 
-        let row = '<' + `${data.msg}` + '>'
+        let row = `${data.msg}`
         document.querySelector('#chat').value += row + '\n';
-
+        // Save the user's current channel on localStorage
         localStorage.setItem('last_channel', data.channel)
-    })
+    });
 
     socket.on('announce message', data => {
 
         let row = '<' + `${data.timestamp}` + '> - ' + '[' + `${data.user}` + ']:  ' + `${data.msg}`
         document.querySelector('#chat').value += row + '\n'
-    })
-
-
+    });
 });
